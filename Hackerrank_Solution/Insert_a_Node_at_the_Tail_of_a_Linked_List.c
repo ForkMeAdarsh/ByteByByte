7089707094b8950b296cf6b800b1fa2cc30ac51c
Adarsh <adarshdeepsingh0@gmail.com>
2024-07-22 17:13:52 +0530

    modified

--- Hackerrank_Solution/Insert_a_Node_at_the_Tail_of_a_Linked_List.c
+++ Hackerrank_Solution/Insert_a_Node_at_the_Tail_of_a_Linked_List.c
@@ -1,50 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct SinglyListNode
+struct SinglyLinkedListNode
 {
     int data;
-    struct SinglyListNode *next;
+    struct SinglyLinkedListNode *next;
 };
 
-void printLinkedList(struct SinglyListNode *ptr)
+void printLinkedList(struct SinglyLinkedListNode *head)
 {
     int i = 0;
-    while (ptr != NULL)
+    while (head != NULL)
     {
-        printf("Element%d: %d\n", i+1, ptr->data);
-        ptr = ptr->next;
+        printf("Element%d: %d\n", i+1, head->data);
+        head = head->next;
         i++;
     }
 }
 
-void InsertAtEnd(struct SinglyListNode *head, int data)
+struct SinglyLinkedListNode *insertNodeAtTail(struct SinglyLinkedListNode *head, int data)
 {
-    struct SinglyListNode *newLast = (struct SinglyListNode *)malloc(sizeof(struct SinglyListNode));
+    struct SinglyLinkedListNode *newNode = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
 
     if (head == NULL)
     {
-        head = newLast;
+        head = newNode;
     }
     else
     {
-        struct SinglyListNode *p = head;
-        while (p->next != NULL)
+        struct SinglyLinkedListNode *newLast = head;
+        while (newLast->next != NULL)
         {
-            p = p->next;
+            newLast = newLast->next;
         }
-        newLast->data = data;
-        newLast->next = p->next;
-        p->next = newLast;
+        newNode->data = data;
+        newNode->next = newLast->next;
+        newLast->next = newNode;
     }
+    return head;
 }
 
 int main()
 {
-    struct SinglyListNode *head = (struct SinglyListNode *)malloc(sizeof(struct SinglyListNode));
-    struct SinglyListNode *second = (struct SinglyListNode *)malloc(sizeof(struct SinglyListNode));
-    struct SinglyListNode *third = (struct SinglyListNode *)malloc(sizeof(struct SinglyListNode));
-    struct SinglyListNode *fourth = (struct SinglyListNode *)malloc(sizeof(struct SinglyListNode));
+    struct SinglyLinkedListNode *head = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
+    struct SinglyLinkedListNode *second = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
+    struct SinglyLinkedListNode *third = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
+    struct SinglyLinkedListNode *fourth = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
 
     head->data = 3;
     head->next = second;
@@ -61,7 +62,7 @@ int main()
     printf("\nTraversing the linkedlist before insertion\n");
     printLinkedList(head);
     printf("\n");
-    InsertAtEnd(head, 60);
+    head = insertNodeAtTail(head, 60);
     printf("Traversing the linkedlist after insertion\n");
     printLinkedList(head);
     printf("\n");
