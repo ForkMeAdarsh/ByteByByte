e79e40c8af58184a4d77db9b9ecfd2550ddbec59
Adarsh <adarshdeepsingh0@gmail.com>
2024-07-22 16:57:19 +0530

    new file

---
+++ Hackerrank_Solution/Insert_a_node_at_the_head_of_a_linked_list.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct SinglyLinkedListNode
+{
+    int data;
+    struct SinglyLinkedListNode *next;
+};
+
+void printLinkedList(struct SinglyLinkedListNode *head)
+{
+    int i = 0;
+    while (head != NULL)
+    {
+        printf("Element%d: %d\n", i+1, head->data);
+        head = head->next;
+        i++;
+    }
+}
+
+struct SinglyLinkedListNode *insertNodeAtHead(struct SinglyLinkedListNode *llist, int data)
+{
+    struct SinglyLinkedListNode *newNode = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
+
+    if (llist == NULL)
+    {
+        llist = newNode;
+        return llist;        
+    }
+    else
+    {
+        newNode->data = data;
+        newNode->next = llist;
+        llist = newNode;
+        return llist;        
+    }
+}
+
+int main()
+{
+    struct SinglyLinkedListNode *head = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
+    struct SinglyLinkedListNode *second = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
+    struct SinglyLinkedListNode *third = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
+    struct SinglyLinkedListNode *fourth = (struct SinglyLinkedListNode *)malloc(sizeof(struct SinglyLinkedListNode));
+
+    head->data = 3;
+    head->next = second;
+
+    second->data = 11;
+    second->next = third;
+
+    third->data = 50;
+    third->next = fourth;
+
+    fourth->data = 41;
+    fourth->next = NULL;
+
+    printf("\nTraversing the linkedlist before insertion\n");
+    printLinkedList(head);
+    printf("\n");
+    head = insertNodeAtHead(head, 60);
+    printf("Traversing the linkedlist after insertion\n");
+    printLinkedList(head);
+    printf("\n");
+    return 0;
+}
