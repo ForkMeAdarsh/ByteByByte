c0825ddd826cddbe3a8909c0794e3f486d924c35
Adarsh <adarshdeepsingh0@gmail.com>
2024-08-07 17:13:25 +0530

    new file

---
+++ DSA_Codes/PostOrderTraversal.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data)
+{
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void postOrderTraversal(struct Node* root)
+{
+    if (root != NULL)
+    {
+        postOrderTraversal(root->left);
+        postOrderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main()
+{
+    struct Node* p = createNode(4);
+    struct Node* p1 = createNode(1);
+    struct Node* p2 = createNode(6);
+    struct Node* p3 = createNode(5);
+    struct Node* p4 = createNode(2);
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    postOrderTraversal(p);
+
+    return 0;
+}
